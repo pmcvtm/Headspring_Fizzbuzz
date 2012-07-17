a46c0b40bfa41423164cf161800e242fb513e62f
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2012-07-17 15:14:27 -0500

    "Classic FizzBuzz" Console Solution
    Set up VS C# Express and this GitHub thing; completed first part of
    assignment

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    //Simple Console Solution
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pretty straightforward:
+            for (int i = 1; i <= 100; i++)  //loop from 1 to 100
+            {
+                if (i % 15 == 0)            //divisible by 15 (== 5*3)
+                    Console.Write("FizzBuzz ");
+                else if (i % 3 == 0)        // or divisible by 3
+                    Console.Write("Fizz ");
+                else if (i % 5 == 0)        // or divisible by 5
+                    Console.Write("Buzz ");
+                else
+                    Console.Write(i + " "); // the rest of them
+            }
+        }
+    }
+}
